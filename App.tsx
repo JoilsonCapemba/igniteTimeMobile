690032ca89fa7aea659c28557e3feabb9d56de6f
JoilsonCapemba <83504861+JoilsonCapemba@users.noreply.github.com>
2024-03-24 14:55:27 +0100

    configurando o babel-plugin-resolver e o syled-component

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
+import { Groups } from '@screens/Groups';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <h1>Ignite Time</h1>
+    <Groups />
   );
 }
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+
+export function Groups(){
+  return(
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{color: 'black' }}>Group</Text>
+    </View>
+  )
+}
